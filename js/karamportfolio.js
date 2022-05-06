4be6516f51e6731348ed3a847b2b208799a39c60
Karam Alsaidi <karamalsaidi@gmail.com>
2022-05-07 02:20:33 +0300

    edit progress

--- js/karamportfolio.js
+++ js/karamportfolio.js
@@ -40,4 +40,24 @@ function activeChange(index) {
       backDelay: 2000
     });
   }
-})()
+});
+
+
+
+// =============== progress
+
+let progressSpans = document.querySelectorAll(".progress span");
+
+let aboutSection = document.querySelector(".about");
+
+
+window.onscroll = function () {
+  // Skills Animate Width
+  if (window.scrollY >= aboutSection.offsetTop) {
+    progressSpans.forEach((span) => {
+      span.style.width = span.dataset.progres;
+    });
+  }
+
+};
+
